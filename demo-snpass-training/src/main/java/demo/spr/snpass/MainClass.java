f920b9e7ad4ac847aad6e0bdbd4af97bd2f4137e
AnitaNagar <anita.kumari@crest.in>
2021-04-28 10:32:42 +0530

    Commit to trigger build

--- demo-snpass-training/src/main/java/demo/spr/snpass/MainClass.java
+++ demo-snpass-training/src/main/java/demo/spr/snpass/MainClass.java
@@ -3,6 +3,6 @@ package demo.spr.snpass;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Demo Project is running");
+        System.out.println("Demo Project is running triiger");
     }
 }
