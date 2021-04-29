2a89b18c274ba22245dd1024101edb598a2214fa
AnitaNagar <anita.kumari@crest.in>
2021-04-29 09:54:33 +0530

    Added spring boot annotation

--- demo-snpass-training/src/main/java/demo/spr/snpass/MainClass.java
+++ demo-snpass-training/src/main/java/demo/spr/snpass/MainClass.java
@@ -1,8 +1,10 @@
 package demo.spr.snpass;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+@SpringBootApplication
 public class MainClass {
     public static void main(String[] args) {
         SpringApplication.run(MainClass.class, args);
